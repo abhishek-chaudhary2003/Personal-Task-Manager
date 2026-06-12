7fd66928b9187ab439837738d441f858f3f886f4
Abhishek Chauhary <chaudharyabhishekmp@gmail.com>
2026-06-12 18:34:32 +0530

    Refactor task controller to improve error handling and response messages

--- Server/controllers/taskController.js
+++ Server/controllers/taskController.js
@@ -1,35 +1,99 @@
+import { readJSON, writeJSON } from "../utils/fileStore.js";
+import { v4 as uuidv4 } from "uuid";
+const TASK_FILE = "./data/tasks.json";
+
 export const getAllTasks = async (req, res) => {
   try {
-    res.json({
-      message: "Get all tasks",
+    const tasks = await readJSON(TASK_FILE);
+
+    const sortedTasks = tasks.sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+    );
+
+    res.status(200).json({
+      sortedTasks,
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({
-      message: "Server error",
+      message: "Failed to fetch tasks",
     });
   }
 };
 
 export const createTask = async (req, res) => {
   try {
-    res.json({
-      message: "Create task",
+    const { title, descreption, dueDate } = req.body;
+
+    if (!title?.trim()) {
+      return res.status(400).json({
+        message: "Title is required",
+      });
+    }
+
+    const tasks = await readJSON(TASK_FILE);
+    const newTask = {
+      id: uuidv4(),
+      title: title.trim(),
+      descreption: descreption?.trim() || "",
+      dueDate: dueDate || null,
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    tasks.push(newTask);
+    await writeJSON(TASK_FILE, tasks);
+
+    res.status(200).json({
+      message: "Task Created Successfully",
+      tasks: newTask,
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({
-      message: "Server error",
+      message: "Failed to create task",
     });
   }
 };
 
 export const updateTask = async (req, res) => {
   try {
-    res.json({
-      message: "Update task",
+    const { id } = req.params;
+    const { title, descreption, dueDate } = req.body();
+
+    if (!title?.trim()) {
+      return res.status(400).json({
+        message: "Title is required",
+      });
+    }
+
+    const tasks = await readJSON(TASK_FILE);
+
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+
+    if ((taskIndex = -1)) {
+      return res.status(400).json({
+        message: "Task dose not exist",
+      });
+    }
+
+    tasks[taskIndex] = {
+      ...tasks[taskIndex],
+      title: title.trim(),
+      descreption: descreption?.trim() || "",
+      dueDate: dueDate || null,
+    };
+
+    await writeJSON(TASK_FILE, tasks);
+
+    res.status(200).json({
+      message: "Task Updated successfully",
+      tasks: task[taskIndex],
     });
   } catch (error) {
+    console.error(error);
+
     res.status(500).json({
-      message: "Server error",
+      message: "Failed to update task",
     });
   }
 };

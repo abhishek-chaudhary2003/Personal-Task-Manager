5fcd4f7b1b0142633673530fff0de7f99ba7776c
Abhishek Chauhary <chaudharyabhishekmp@gmail.com>
2026-06-13 12:23:28 +0530

    fix: update CORS configuration to include methods for API requests

--- Server/server.js
+++ Server/server.js
@@ -8,12 +8,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
+app.use(
+  cors({
     origin: [
       "http://localhost:5173",
-      "https://task-manager-app-frontend.netlify.app/"
-    ]
-  }));
+      "https://task-manager-app-frontend.netlify.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);

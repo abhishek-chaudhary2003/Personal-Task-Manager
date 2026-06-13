b05dec1767906b11b6acb8ae5e0d37f017fb096c
Abhishek Chauhary <chaudharyabhishekmp@gmail.com>
2026-06-13 12:04:20 +0530

    fix: update CORS configuration to specify allowed origins

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+      "https://task-manager-app-frontend.netlify.app/"
+    ]
+  }));
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
